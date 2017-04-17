9a89e2448ffc6a61f6a986cbfff824d769eb2ab4
gustavo7lagoas <gustavo7lagoas@gmail.com>
2017-04-17 20:11:51 -0300

    Front page html generated from a template

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -26,13 +26,14 @@ class NewVisitorTest(unittest.TestCase):
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
-        inputbox.send_keys(Keys.Enter)
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "The searched item is not in the To-do list"
         )
         self.fail('Finish the test!')
 
