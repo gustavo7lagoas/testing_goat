b79412ee2539c1619821b26dd1e43c58a5de93ec
gustavo7lagoas <gustavo7lagoas@gmail.com>
2017-04-18 20:02:35 -0300

    Refactors test by adding helper function and remove duplication

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Gustavo has heard about a cool new online to-do app. He goes
         # to check out its homepage
@@ -37,24 +42,17 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy food', [row.text for row in rows])
-
+        self.check_for_row_in_list_table('1: Buy food')
         # There is still a text box inviting him to add another item. He
-        # enters "Use mushrooms to a strogonoff"
+        # enters "User mushrooms to do a strogonoff"
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use mushrooms to a strogonoff')
+        inputbox.send_keys('User mushrooms to do a strogonoff')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         # The page updates again, and now shows both items on his lists
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy food', [row.text for row in rows])
-        self.assertIn(
-            '2: Use mushrooms to a strogonoff',
-            [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy food')
+        self.check_for_row_in_list_table('2: User mushrooms to do a strogonoff')
 
         # Gustavo wonders wheter the site will remember his list. Then he sees
         # that the site has generated a unique URL for him -- there is some
