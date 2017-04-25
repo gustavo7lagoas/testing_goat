8987d00965ab56c231cd42f5448a603d5853c34d
gustavo7lagoas <gustavo7lagoas@gmail.com>
2017-04-25 17:33:14 -0300

    Redirect after POST and list all saved items in template

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -6,7 +6,7 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
 
     def tearDown(self):
         self.browser.quit()
